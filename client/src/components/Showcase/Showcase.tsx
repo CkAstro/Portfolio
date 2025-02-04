7c6478c9e59bc86a73dea79e6a2a29d869bcf6ea
chris <ckastro.git@gmail.com>
2025-02-03 19:32:02 -0500

    updated portfolio layout, still needs work

--- client/src/components/Showcase/Showcase.tsx
+++ client/src/components/Showcase/Showcase.tsx
@@ -5,17 +5,28 @@ interface ShowcaseProps {
    tech: string | React.ReactElement;
    description: string | React.ReactElement;
    image: React.ReactElement;
+   onPointerEnter: () => void;
+   onPointerLeave: () => void;
+   isActive: boolean;
 }
 
-export const Showcase: React.FC<ShowcaseProps> = ({ header, tech, description, image }) => {
-   return (
-      <div className={styles.showcase}>
-         <div className={styles.showcase__image}>{image}</div>
-         <div className={styles.showcase__content}>
-            <h2>{header}</h2>
-            <h3>{tech}</h3>
-            <p>{description}</p>
-         </div>
+export const Showcase: React.FC<ShowcaseProps> = ({
+   header,
+   tech,
+   description,
+   image,
+   onPointerEnter,
+   onPointerLeave,
+   isActive,
+}) => (
+   <div className={styles.showcase} onPointerEnter={onPointerEnter} onPointerLeave={onPointerLeave}>
+      <div className={styles.showcase__image}>{image}</div>
+      <div
+         className={[styles.showcase__content, isActive ? styles.active : styles.hidden].join(' ')}
+      >
+         <h2>{header}</h2>
+         <h3>{tech}</h3>
+         <p>{description}</p>
       </div>
-   );
-};
+   </div>
+);
--- client/src/content/Portfolio/Portfolio.tsx
+++ client/src/content/Portfolio/Portfolio.tsx
@@ -1,26 +1,38 @@
+import { useState } from 'react';
 import { Section, Showcase, LazyImage } from '@/components';
 import { portfolioItems } from './portfolioItems';
 import type { SectionProps } from '../types';
 import styles from '../content.module.scss';
 
-export const Portfolio: React.FC<SectionProps> = ({ id }) => (
-   <Section
-      id={id}
-      header={
-         <h1>
-            Some <span style={{ fontWeight: 900 }}>Projects</span>
-         </h1>
-      }
-      className={[styles.contentContainer, styles.portfolio].join(' ')}
-   >
-      {portfolioItems.map(({ header, tech, description, imageUri, altText, imageSizes }) => (
-         <Showcase
-            key={altText}
-            header={header}
-            tech={tech}
-            description={description}
-            image={<LazyImage sizes={imageSizes} altText={altText} imageUri={imageUri} />}
-         />
-      ))}
-   </Section>
-);
+export const Portfolio: React.FC<SectionProps> = ({ id }) => {
+   const [activeShowcase, setActiveShowcase] = useState(-1);
+
+   return (
+      <Section
+         id={id}
+         header={
+            <h1>
+               Some <span style={{ fontWeight: 900 }}>Projects</span>
+            </h1>
+         }
+         className={[styles.contentContainer, styles.portfolio].join(' ')}
+      >
+         <div className={styles.imageContainer}>
+            {portfolioItems.map(
+               ({ header, tech, description, imageUri, altText, imageSizes }, index) => (
+                  <Showcase
+                     key={altText}
+                     onPointerEnter={(): void => setActiveShowcase(index)}
+                     onPointerLeave={(): void => setActiveShowcase(-1)}
+                     isActive={activeShowcase === index}
+                     header={header}
+                     tech={tech}
+                     description={description}
+                     image={<LazyImage sizes={imageSizes} altText={altText} imageUri={imageUri} />}
+                  />
+               )
+            )}
+         </div>
+      </Section>
+   );
+};
